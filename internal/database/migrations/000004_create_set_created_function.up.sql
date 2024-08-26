3e6edadf90541e75dfec2b95169d8ae516bf4bdc
Javier Galarza <jegj57@gmail.com>
2024-08-26 08:12:42 -0500

    fix: function on trigger

--- internal/database/migrations/000004_create_set_created_function.up.sql
+++ internal/database/migrations/000004_create_set_created_function.up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 CREATE OR REPLACE FUNCTION linktly.set_created_at() RETURNS TRIGGER AS $$
 BEGIN
-    NEW.created_at := uuid_v8_to_timestamptz(NEW.id);
+    NEW.created_at := uuid_v7_to_timestamptz(NEW.id);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;

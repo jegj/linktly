b91edd43e2a659cb3355b626b19677b11abda191
Javier Galarza <jegj57@gmail.com>
2024-08-26 21:07:40 -0500

    feat: initial server setup

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	route "github.com/jegj/linktly/internal/api/routes"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Heartbeat("/ping"))
+
+	r.Mount("/test", route.PingRoutes())
+	// r.Mount("/api/v1", route.PingRoutes())
+
+	// Start the HTTP server
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module linktly
+module github.com/jegj/linktly
 
 go 1.23.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
+)

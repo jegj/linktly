92fc1ad673f321ce62c2a3a3a96b8c5a12747af1
Javier Galarza <jegj57@gmail.com>
2024-11-05 06:22:45 -0500

    chore: order fields

--- internal/api/domain/links/types.go
+++ internal/api/domain/links/types.go
@@ -8,14 +8,14 @@ import (
 type Link struct {
 	CreatedAt   time.Time  `db:"created_at" json:"created_at"`
 	UpdatedAt   *time.Time `db:"updated_at" json:"updated_at"`
+	FolderId    *string    `db:"folder_id" validate:"omitempty,uuid" json:"parent_folder_id"`
+	ExpiresAt   *time.Time `db:"expires_at" json:"expires_at"`
 	Id          string     `db:"id" json:"id"`
 	LinktlyUrl  string     `db:"linktly_url" json:"linktly_url"`
 	Url         string     `db:"url" json:"url"`
 	Name        string     `db:"name" validate:"required,min=3,max=255" json:"name"`
-	FolderId    *string    `db:"folder_id" validate:"omitempty,uuid" json:"parent_folder_id"`
 	AccountId   string     `db:"account_id" validate:"omitempty,uuid" json:"account_id"`
 	Description string     `db:"description" validate:"min=10" json:"description"`
-	ExpiresAt   *time.Time `db:"expires_at" json:"expires_at"`
 }
 
 type LinkReq struct {

89c784051d754316fc483f0d4f9baab889b28b3b
Javier Galarza <jegj57@gmail.com>
2024-11-07 06:48:17 -0500

    fix: create link repository func

--- internal/api/domain/links/repository.go
+++ internal/api/domain/links/repository.go
@@ -26,7 +26,7 @@ func (repo *PostgresRepository) CreateLink(ctx context.Context, link *Link) (*Li
 	var id string
 	var createdAt time.Time
 
-	query := `INSERT INTO linktly.links(name, description, account_id, folder_id, linktly_url, url, expires_at ) VALUES($1,$2,$3,$4) RETURNING id, created_at`
+	query := `INSERT INTO linktly.links(name, description, account_id, folder_id, linktly_url, url, expires_at ) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING id, created_at`
 	err := repo.store.Source.QueryRow(ctx, query, link.Name, link.Description, link.AccountId, link.FolderId, link.LinktlyUrl, link.Url, link.ExpiresAt).Scan(&id, &createdAt)
 	if err != nil {
 		return nil, linktlyError.PostgresFormatting(err)

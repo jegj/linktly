34e8ab69663dfceea90ee55dbfbcce6438eec5bc
Javier Galarza <jegj57@gmail.com>
2024-11-14 07:40:05 -0500

    feat: retry code generation until valid code

--- internal/api/domain/links/service.go
+++ internal/api/domain/links/service.go
@@ -2,7 +2,12 @@ package links
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"math/rand"
+	"net/http"
+
+	"github.com/jegj/linktly/internal/api/types"
 )
 
 type LinksService struct {
@@ -10,12 +15,26 @@ type LinksService struct {
 }
 
 func (l *LinksService) CreateLink(ctx context.Context, link *Link) (*Link, error) {
-	// TODO: Handle collisions
-	link.LinktlyCode = CreateShortCode()
-	return l.Repository.CreateLink(ctx, link)
+	for {
+		link.LinktlyCode = createShortCode()
+		nlink, err := l.Repository.CreateLink(ctx, link)
+		if err == nil || !isConflictApiError(err) {
+			return nlink, err
+		}
+		msg := fmt.Sprintf("Short code conflict %v, generating a new one", link.LinktlyCode)
+		slog.Info(msg, "error", err.Error(), "code", link.LinktlyCode)
+	}
 }
 
-func CreateShortCode() string {
+func isConflictApiError(err error) bool {
+	if apiErr, ok := err.(types.APIError); ok {
+		return apiErr.StatusCode == http.StatusConflict
+	} else {
+		return false
+	}
+}
+
+func createShortCode() string {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	b := make([]byte, 6)
 	for i := range b {
